a0c1b99fcbe4a1bf45a45a963e7e5645bc54b2b9
yuzuki <youuu.sunnn@gmail.com>
2025-07-27 23:48:58 +0900

    Initial Commit

---
+++ LocationPoster/LocationPosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPosterApp.swift
+//  LocationPoster
+//
+//  Created by 矢口悠月 on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct LocationPosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationPosterTests/LocationPosterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPosterTests.swift
+//  LocationPosterTests
+//
+//  Created by 矢口悠月 on 2025/07/27.
+//
+
+import Testing
+@testable import LocationPoster
+
+struct LocationPosterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
